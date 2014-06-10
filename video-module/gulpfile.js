a0c978f81c331cf722cb98e26329389fc2deb4ec
David Annopolsky <annopolskyd1@uoifpmaewnorton.urbanout.com>
2014-06-10 17:56:30 -0400

    test

--- video-module/gulpfile.js
+++ video-module/gulpfile.js
@@ -1,12 +1,19 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var gutil = require('gulp-util');
 
-gulp.task('default', function() {
-    return gulp.src(['test/test-*.js'], { read: false })
+gulp.task('mocha', function() {
+    return gulp.src(['test/*.js'], {
+            read: false
+        })
         .pipe(mocha({
             reporter: 'spec',
             globals: {
                 should: require('should')
             }
-        }));
+        }))
+        .on('error', gutil.log);
+});
+gulp.task('default', function() {
+    gulp.watch(['test/**'], ['mocha']);
 });
---
+++ video-module/test/test.js
@@ -0,0 +1,5 @@
+describe('videoEmbed', function(){
+  describe('init', function(){
+
+  });
+});
