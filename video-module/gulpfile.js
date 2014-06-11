672d5da1209f02e973b80084e352eb9d60471475
David Annopolsky <annopolskyd1@uoifpmaewnorton.urbanout.com>
2014-06-11 09:02:56 -0400

    fixed the gulpfile, had weird git comments

--- video-module/gulpfile.js
+++ video-module/gulpfile.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-var gutil = require('gulp-util');
-
-gulp.task('mocha', function() {
-    return gulp.src(['test/*.js'], {
-            read: false
-        })
-        .pipe(mocha({
-            reporter: 'spec',
-            globals: {
-                should: require('should')
-            }
-        }))
-        .on('error', gutil.log);
-});
-gulp.task('default', function() {
-    gulp.watch(['test/**'], ['mocha']);
-=======
 'use strict';
 
 var fs = require('fs');
@@ -27,5 +7,5 @@ var tasks = fs.readdirSync(dir);
 // load gulp tasks from directory
 tasks.forEach(function (task) {
     require(dir + task);
->>>>>>> 22b76ab444c782e82da3878d29d2d30908f54255
+
 });
